758d2576adc5f2caaf09144adca4d3f1211b6b9e
Håkan Sundblad <hakan.sundblad@liu.se>
2025-02-22 13:28:39 +0100

    Added GitHub workflow

---
+++ src/pyfolioclient/__version__.py
@@ -0,0 +1,12 @@
+"""__version__.py: Metadata about the pyfolioclient package."""
+
+__title__ = "pyfolioclient"
+__description__ = "Python HTTP for Humans."
+__url__ = "https://requests.readthedocs.io"
+__version__ = "0.1.0"
+# __build__ = 0x023203
+__author__ = "Håkan Sundblad"
+__author_email__ = "hakan.sundblad@liu.se"
+__license__ = "MIT"
+__copyright__ = "Copyright Håkan Sundblad"
+# __cake__ = "\u2728 \U0001f370 \u2728"
