d3c3e967a0629ec2b2a9388c86a877d13e04c7ad
Håkan Sundblad <hakan.sundblad@liu.se>
2025-02-22 13:53:27 +0100

    Working on build congig

--- src/pyfolioclient/__version__.py
+++ src/pyfolioclient/__version__.py
@@ -1,8 +1,8 @@
 """__version__.py: Metadata about the pyfolioclient package."""
 
 __title__ = "pyfolioclient"
-__description__ = "Python HTTP for Humans."
-__url__ = "https://requests.readthedocs.io"
+__description__ = "A Python client for interacting with FOLIO's APIs"
+__url__ = "https://github.com/balljok/pyfolioclient"
 __version__ = "0.1.0"
 # __build__ = 0x023203
 __author__ = "Håkan Sundblad"
